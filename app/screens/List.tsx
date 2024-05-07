e2791024cd90e8e812001fb59592f689eaa99692
Pruthuvi Nugaliyadda <pruthuvipraveen@gmail.com>
2024-05-07 23:12:55 +0530

    Testing routes

---
+++ app/screens/List.tsx
@@ -0,0 +1,13 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const List = () => {
+  return (
+    <View>
+      <Text>List</Text>
+      <Text>New List</Text>
+    </View>
+  );
+};
+
+export default List;
